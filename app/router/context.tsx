542a454141973b49dc920505502eb975d3a50f7f
sam <aquaticcalf@proton.me>
2026-01-03 12:56:49 +0530

    use router

--- app/router/context.tsx
+++ app/router/context.tsx
@@ -2,11 +2,11 @@ import { createContext } from "react"
 
 import type { Screen } from "@/types"
 
-interface RouteContextType {
+interface RouterContextType {
   current: Screen
   params: Record<string, unknown>
   goto: (screen: Screen, params?: Record<string, unknown>) => void
   goback: () => void
 }
 
-export const RouteContext = createContext<RouteContextType | null>(null)
+export const RouterContext = createContext<RouterContextType | null>(null)
